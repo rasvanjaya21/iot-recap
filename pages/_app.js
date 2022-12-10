f41bc7703a2430f87b20f25a57e61fe1dfd7a72c
rasvanjaya21 <mrsejati17@gmail.com>
2022-12-11 06:52:39 +0700

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Router from "next/router";
+import nProgress from "nprogress";
+import "@styles/nprogress.css";
+import "@styles/globals.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React from "react";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	React.useEffect(() => {
+		AOS.init({
+			once: true,
+		});
+	});
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
