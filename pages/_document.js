fa75d5baa2f3b6d6ad6a224c79a846f1e1186421
rasvanjaya21 <mrsejati17@gmail.com>
2022-12-13 15:40:08 +0700

    initial google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,28 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
 	return (
 		<Html style={{ scrollBehavior: "smooth" }}>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<Script 
+				id="google-analytics"
+				strategy="lazyOnload">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</Script>
+
 			<Head>
 				<link rel="icon" href="/favicon.png" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
