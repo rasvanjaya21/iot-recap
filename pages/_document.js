955e9ee5563c00860d0c9124512379e7c33e1ecf
rasvanjaya21 <mrsejati17@gmail.com>
2022-12-13 15:47:31 +0700

    update <3

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,26 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
+
+	const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`;
+
 	return (
 		<Html style={{ scrollBehavior: "smooth" }}>
-			<Script
-				strategy="lazyOnload"
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-			/>
-
-			<Script 
-				id="google-analytics"
-				strategy="lazyOnload">
-				{`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-			</Script>
-
 			<Head>
+				<script async src={gtag} />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+							page_path: window.location.pathname,
+							});
+						`,
+					}}
+				/>
 				<link rel="icon" href="/favicon.png" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
